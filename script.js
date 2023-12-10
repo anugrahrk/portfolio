f8fd0818979a6407a63f6e39e8d15a5a82e819dc
ANUGRAH RK <146818837+anugrahrk@users.noreply.github.com>
2023-12-10 22:49:53 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,107 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+var darkmode= document.getElementById("darkmode");
+darkmode.onclick=function()
+{
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme"))
+    {
+        darkmode.src ="assets/sun.png";  
+        linkedin.src ="assets/linkedindark.png";
+        linkedin1.src ="assets/linkedindark.png";
+        github.src ="assets/githubdark.png";
+        experiences.src ="./assets/experiencedark.png";
+        educations.src ="./assets/educationdark.png";
+        arrowicon.src ="./assets/arrowdark.png";
+        arrowicon1.src ="./assets/arrowdark.png";
+        arrowicon2.src ="./assets/arrowdark.png";
+        checkmark.src ="./assets/checkmarkdark.png";
+        checkmark1.src ="./assets/checkmarkdark.png";
+        checkmark2.src ="./assets/checkmarkdark.png";
+        checkmark3.src ="./assets/checkmarkdark.png";
+        checkmark4.src ="./assets/checkmarkdark.png";
+        checkmark5.src ="./assets/checkmarkdark.png";
+        checkmark6.src ="./assets/checkmarkdark.png";
+        checkmark7.src ="./assets/checkmarkdark.png";
+        checkmark8.src ="./assets/checkmarkdark.png";
+        checkmark9.src ="./assets/checkmarkdark.png";
+        email.src ="./assets/emaildark.png";
+        
+    }
+    else{
+        darkmode.src ="assets/moon.png";
+        linkedin.src ="assets/linkedin.png";
+        linkedin1.src ="assets/linkedin.png";
+        github.src ="assets/github.png";
+        experiences.src ="./assets/experience.png";
+        educations.src ="./assets/education.png";
+        arrowicon.src ="./assets/arrow.png";
+        arrowicon1.src ="./assets/arrow.png";
+        arrowicon2.src ="./assets/arrow.png";
+        checkmark.src ="./assets/checkmark.png";
+        checkmark1.src ="./assets/checkmark.png";
+        checkmark2.src ="./assets/checkmark.png";
+        checkmark3.src ="./assets/checkmark.png";
+        checkmark4.src ="./assets/checkmark.png";
+        checkmark5.src ="./assets/checkmark.png";
+        checkmark6.src ="./assets/checkmark.png";
+        checkmark7.src ="./assets/checkmark.png";
+        checkmark8.src ="./assets/checkmark.png";
+        checkmark9.src ="./assets/checkmark.png";
+        email.src ="./assets/email.png";
+    }
+}
+var darkmode1= document.getElementById("darkmode1");
+darkmode1.onclick=function()
+{
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme"))
+    {
+        darkmode1.src ="assets/sun.png";  
+        linkedin.src ="assets/linkedindark.png";
+        linkedin1.src ="assets/linkedindark.png";
+        github.src ="assets/githubdark.png";
+        experiences.src ="./assets/experiencedark.png";
+        educations.src ="./assets/educationdark.png";
+        arrowicon.src ="./assets/arrowdark.png";
+        arrowicon1.src ="./assets/arrowdark.png";
+        arrowicon2.src ="./assets/arrowdark.png";
+        checkmark.src ="./assets/checkmarkdark.png";
+        checkmark1.src ="./assets/checkmarkdark.png";
+        checkmark2.src ="./assets/checkmarkdark.png";
+        checkmark3.src ="./assets/checkmarkdark.png";
+        checkmark4.src ="./assets/checkmarkdark.png";
+        checkmark5.src ="./assets/checkmarkdark.png";
+        checkmark6.src ="./assets/checkmarkdark.png";
+        checkmark7.src ="./assets/checkmarkdark.png";
+        checkmark8.src ="./assets/checkmarkdark.png";
+        checkmark9.src ="./assets/checkmarkdark.png";
+        email.src ="./assets/emaildark.png";  
+    }
+    else{
+        darkmode1.src ="assets/moon.png";
+        linkedin.src ="assets/linkedin.png";
+        linkedin1.src ="assets/linkedin.png";
+        github.src ="assets/github.png";
+        experiences.src ="./assets/experience.png";
+        educations.src ="./assets/education.png";
+        arrowicon.src ="./assets/arrow.png";
+        arrowicon1.src ="./assets/arrow.png";
+        arrowicon2.src ="./assets/arrow.png";
+        checkmark.src ="./assets/checkmark.png";
+        checkmark1.src ="./assets/checkmark.png";
+        checkmark2.src ="./assets/checkmark.png";
+        checkmark3.src ="./assets/checkmark.png";
+        checkmark4.src ="./assets/checkmark.png";
+        checkmark5.src ="./assets/checkmark.png";
+        checkmark6.src ="./assets/checkmark.png";
+        checkmark7.src ="./assets/checkmark.png";
+        checkmark8.src ="./assets/checkmark.png";
+        checkmark9.src ="./assets/checkmark.png";
+        email.src ="./assets/email.png";
+    }
+}
